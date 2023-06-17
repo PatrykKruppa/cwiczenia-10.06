2c260b01a3336791643886dfb674e5e3d4492b40
PatrykKruppa <128226968+PatrykKruppa@users.noreply.github.com>
2023-06-17 10:30:36 +0200

    Add files via upload

---
+++ 6 - silnia liczby n.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę x: ");
+        int x = int.Parse(Console.ReadLine());
+
+        int n = 0;
+        int faktorial = 1;
+
+        while (faktorial <= x)
+        {
+            n++;
+
+            if (faktorial == x)
+            {
+                Console.WriteLine("Liczba " + x + " jest silnią liczby " + n);
+                return;
+            }
+
+            faktorial *= n;
+        }
+
+        Console.WriteLine("Liczba " + x + " nie jest silnią żadnej liczby");
+
+        Console.ReadLine();
+    }
+}
