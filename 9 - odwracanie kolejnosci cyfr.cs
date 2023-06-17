e336bc1ffc32c90a01d4ce312e8eb0ea23626ac0
PatrykKruppa <128226968+PatrykKruppa@users.noreply.github.com>
2023-06-17 10:54:10 +0200

    Add files via upload

---
+++ 9 - odwracanie kolejnosci cyfr.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Jest to program, który odwraca kolejność podanej liczby: ");
+        Console.Write("Podaj liczbę: ");
+        int liczba = int.Parse(Console.ReadLine());
+
+        int odwroconaLiczba = OdwrocKolejnoscCyfr(liczba);
+
+        Console.WriteLine("Liczba po odwróceniu: " + odwroconaLiczba);
+
+        Console.ReadLine();
+    }
+
+    static int OdwrocKolejnoscCyfr(int liczba)
+    {
+        int odwroconaLiczba = 0;
+
+        while (liczba > 0)
+        {
+            int cyfra = liczba % 10;
+            odwroconaLiczba = (odwroconaLiczba * 10) + cyfra;
+            liczba /= 10;
+        }
+
+        return odwroconaLiczba;
+    }
+}
