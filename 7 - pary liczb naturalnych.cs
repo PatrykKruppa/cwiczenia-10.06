bc6d7dc51f926a12bfe13dbf7122782cedb5e2da
PatrykKruppa <128226968+PatrykKruppa@users.noreply.github.com>
2023-06-17 10:35:21 +0200

    Add files via upload

---
+++ 7 - pary liczb naturalnych.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Pary liczb naturalnych, których suma wynosi " + n + " to:");
+
+        for (int i = 1; i <= n / 2; i++)
+        {
+            int j = n - i;
+            Console.WriteLine("(" + i + ", " + j + ")");
+        }
+
+        Console.ReadLine();
+    }
+}
