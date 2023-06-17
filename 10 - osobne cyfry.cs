6d935ac909c0a9414e1d48dcc5f9b3ef4454814e
PatrykKruppa <128226968+PatrykKruppa@users.noreply.github.com>
2023-06-17 11:01:45 +0200

    Add files via upload

---
+++ 10 - osobne cyfry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Jest to program, który wypisuje osobno liczby naturalne dowolnej liczby która posiada przynajmniej 3 cyfry");
+        Console.Write("Podaj liczbę naturalną: ");
+        int liczba = int.Parse(Console.ReadLine());
+
+        if (liczba < 100)
+        {
+            Console.WriteLine("Podana liczba posiada mniej niż 3 cyfry.");
+        }
+        else
+        {
+            Console.WriteLine("Cyfry liczby " + liczba + ":");
+
+            while (liczba > 0)
+            {
+                int cyfra = liczba % 10;
+                Console.WriteLine(cyfra);
+                liczba /= 10;
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
